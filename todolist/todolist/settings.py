65e08eb8b20292786b006ee5c6302cf1b5661074
im-ayush <36653374+im-ayush@users.noreply.github.com>
2020-09-01 21:35:12 +0530

    secret key removed

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#uqfcidql^$y_o_v*ut860++5d8km353r=cnr#lzt_gor*)_a$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
